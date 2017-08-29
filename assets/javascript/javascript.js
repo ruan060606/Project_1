331bbbd3713d50e4152ffb1d96fba9d2e4c5c1a1
Darren Kwong <darrenkwong@darrens-mbp.attlocal.net>
2017-08-29 14:22:15 -0700

    removed google, fb, twitter until fixed

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -38,9 +38,9 @@ var uiConfig = {
         signInSuccessUrl: '<url-to-redirect-to-on-success>',
         signInOptions: [
           // Leave the lines as is for the providers you want to offer your users.
-          firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-          firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-          firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+          // firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+          // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+          // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
           {
             provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
             // Whether the display name should be displayed in the Sign Up page.
