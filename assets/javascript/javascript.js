cf5c480973650a51b7b6d6e5c0a1a7771eca58ef
Matthew Hoe <matthewh913@gmail.com>
2017-08-28 21:25:11 -0700

    add form data passed into ajax request

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,22 +1,38 @@
+
 (function(){
 //example using places api
 
+$("#button_submit").on("click", function(){
+
 var API_KEY = "AIzaSyCQPkqDoLqZjqpqhqnnRyE79yUe0omijso";
 //https://stackoverflow.com/questions/45185061/google-places-api-cors
-var PROXY_URL = 'https://cors-anywhere.herokuapp.com/'
+var PROXY_URL = 'https://cors-anywhere.herokuapp.com/';
+var caloriesToBurn =  parseInt($("#calorie_field").val().trim());
+var weight = parseInt($("#weight_field").val().trim())
+// http://www.livestrong.com/article/314404-how-many-calories-do-you-lose-per-mile/
+var caloriesPerMile = weight * .75
 
-var radius = "5000";
+var milesToRun = caloriesToBurn / caloriesPerMile;
 var type="museum";
 var location = "51.503186,-0.126446";
 var url = PROXY_URL
 		+"https://maps.googleapis.com/maps/api/place/radarsearch/"
         +"json?location=" + location + "&"
-        +"radius=" + radius + "&" 
+        +"radius=" + milesToRun + "&" 
         +"type=" + type + "&"
         +"key="+API_KEY;
 
+        console.log("url",url)
 
-$.ajax({method:"GET", 
-		url: url}).done(function(data){console.log(data)})
+
+	$.ajax({method:"GET", 
+			url: url}).done(function(data){
+				console.log("done")
+				console.log(data)
+				})
+
+
+})
+
 
 })()
