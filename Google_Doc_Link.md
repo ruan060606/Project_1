9907956dfa98e073fa5b236435a77ce013f34ded
Darren Kwong <darrenkwong@darrens-mbp.attlocal.net>
2017-08-29 12:08:50 -0700

    testing firebase-auth-ui

---
+++ Google_Doc_Link.md
@@ -0,0 +1,2 @@
+# Project_1
+https://docs.google.com/document/d/1Mk4AfuPy2G9jJaGGO69mh7-2h1auw_wtOPIRVsdKO8A/edit
--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -13,6 +13,26 @@ firebase.initializeApp(config);
 // Create a variable to reference the database
 var database = firebase.database();
 
+var uiConfig = {
+  signInSuccessUrl: '<url-to-redirect-to-on-success>',
+  signInOptions: [
+    // Leave the lines as is for the providers you want to offer your users.
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+    // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+    // firebase.auth.GithubAuthProvider.PROVIDER_ID,
+    // firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    // firebase.auth.PhoneAuthProvider.PROVIDER_ID
+  ],
+  // Terms of service url.
+  tosUrl: '<your-tos-url>'
+};
+
+// Initialize the FirebaseUI Widget using Firebase.
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+// The start method will wait until the DOM is loaded.
+ui.start('#firebaseui-auth-container', uiConfig);
+
 //example using places api
 
 var API_KEY = "AIzaSyCQPkqDoLqZjqpqhqnnRyE79yUe0omijso";
