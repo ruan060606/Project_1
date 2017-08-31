eebb21050b86e3662a3001a86997a9ea73189ac7
Matthew Hoe <matthewh913@gmail.com>
2017-08-30 20:09:37 -0700

    user distance used in dist calculation

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,38 +1,52 @@
 
 (function(){
-//example using places api
+//get user location
+if(navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(function(geoLocation){
+    var latitude = geoLocation.coords.latitude;
+    var longitude = geoLocation.coords.longitude
+    var location = latitude + "," + longitude;
+    //example using places api
 
-$("#button_submit").on("click", function(){
+    $("#button_submit").on("click", function(e){
+      e.preventDefault()
 
-var API_KEY = "AIzaSyCQPkqDoLqZjqpqhqnnRyE79yUe0omijso";
-//https://stackoverflow.com/questions/45185061/google-places-api-cors
-var PROXY_URL = 'https://cors-anywhere.herokuapp.com/';
-var caloriesToBurn =  parseInt($("#calorie_field").val().trim());
-var weight = parseInt($("#weight_field").val().trim())
-// http://www.livestrong.com/article/314404-how-many-calories-do-you-lose-per-mile/
-var caloriesPerMile = weight * .75
+    var API_KEY = "AIzaSyCQPkqDoLqZjqpqhqnnRyE79yUe0omijso";
+    //https://stackoverflow.com/questions/45185061/google-places-api-cors
+    var PROXY_URL = 'https://cors-anywhere.herokuapp.com/';
+    var caloriesToBurn =  parseInt($("#calorie_field").val().trim());
+    var weight = parseInt($("#weight_field").val().trim())
+    // http://www.livestrong.com/article/314404-how-many-calories-do-you-lose-per-mile/
+    var caloriesPerMile = weight * .75
 
-var milesToRun = caloriesToBurn / caloriesPerMile;
-var type="museum";
-var location = "51.503186,-0.126446";
-var url = PROXY_URL
-		+"https://maps.googleapis.com/maps/api/place/radarsearch/"
-        +"json?location=" + location + "&"
-        +"radius=" + milesToRun + "&" 
-        +"type=" + type + "&"
-        +"key="+API_KEY;
+    var milesToRun = caloriesToBurn / caloriesPerMile;
+    console.log("milesToRUn", milesToRun)
+    var type="museum";
+    var url = PROXY_URL
+        +"https://maps.googleapis.com/maps/api/place/radarsearch/"
+            +"json?location=" + location + "&"
+            +"radius=" + milesToRun + "&" 
+            +"type=" + type + "&"
+            +"key="+API_KEY;
 
-        console.log("url",url)
-
-
-	$.ajax({method:"GET", 
-			url: url}).done(function(data){
-				console.log("done")
-				console.log(data)
-				})
+            console.log("url",url)
 
 
-})
+
+      $.ajax({method:"GET", 
+          url: url}).done(function(data){
+            console.log("done")
+            console.log(data)
+            })
+
+
+    })
+
+
+  })
+  
+
+}
 
 
 })()
